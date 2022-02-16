086e4b8da842aa2d7ec9de33960f227eb0eef450
EarlPappi <earlpappi@gmail.com>
2022-02-16 00:17:12 +0000

    fixed a little js for the work tab

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const navs = document.querySelectorAll('#navi');
 
-navs.forEach(function(nav){
-    nav.addEventListener('click', function(e){
+navs.forEach(function (nav) {
+    nav.addEventListener('click', function (e) {
         // e.preventDefault()
     })
 })
+
+const workCard = document.querySelectorAll('#work-card');
+const workLink = document.querySelectorAll('#work-link');
+
+console.log(workLink);
+
+
+
+workCard.forEach(function (card) {
+    card.addEventListener('mouseenter', function (e) {
+        workLink.forEach(function (link) {
+            if (link.style.display == 'none') {
+                link.style.display = 'block'
+            } else {
+                link.style.display = 'none';
+            }
+        }
+        );
+
+        //         if(workLink.getElementsByClassName.display == 'none'){
+        //             workLink.getElementsByClassName.display == 'block'
+        // 
+        //             console.log(workLink)
+        //         }
+    });
+});
